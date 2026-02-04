762be6c99a62f0ca39442c84877f8d93c6611b77
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-04 19:03:34 +0800

    build: move to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/iconify.ts'],
+  dts: true,
+  publint: true,
+  attw: {
+    profile: 'node16',
+  },
+})
