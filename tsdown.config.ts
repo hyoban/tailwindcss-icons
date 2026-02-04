4639df6d8a4be28b1a04266243d09f6bbf78d49f
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-04 19:19:23 +0800

    build: add cjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/iconify.ts'],
   dts: true,
+  format: ['cjs', 'esm'],
   publint: true,
   // attw: {
   //   profile: 'node16',
